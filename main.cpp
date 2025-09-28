28175ff748c99d189f7d2e7263a2951e81081e59
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-28 16:27:20 -0700

    update

--- main.cpp
+++ main.cpp
@@ -4,3 +4,26 @@
 #include <iostream>
 
 using namespace std; 
+
+class Color {
+    private: 
+        int Red, Green, Blue; 
+    public:
+        // Getters and Setters methods
+        void setRed(int r) { Red = r;}
+        void setGreen(int g) { Green = g;}
+        void setBlue(int b) { Blue = b;}
+        int getRed() const { return Red;}
+        int getGreen() const { return Green;}
+        int getBlue() const { return Blue;}
+        // Display method
+        void display() const { 
+            cout << "RGB(Red:" << Red << ", Green:" << Green << ", Blue:" << Blue << ")" << endl; 
+        }
+};
+
+int main() {
+    
+
+    return 0;
+}
